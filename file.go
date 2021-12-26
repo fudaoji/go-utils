063dd2b057f334c10faf457454192fe488279ca4
fudaoji <fudaoji@gmail.com>
2021-12-26 20:16:17 +0800

    Create file.go

---
+++ file.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"bufio"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+//FetchFile 拉取远程图片
+func FetchFile(fileUrl string, destPath string, fileName string) (*os.File, error) {
+	destPath = MakePath(destPath, 0766)
+	//imgUrl := "https://cdn2.jianshu.io/assets/default_avatar/9-cceda3cf5072bcdd77e8ca4f21c40998.jpg"
+
+	if len(fileName) < 1 {
+		fileName = path.Base(fileUrl)
+	}
+	res, err := http.Get(fileUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	// 获得get请求响应的reader对象
+	//fmt.Printf("网络文件大小为[%v]:", res.ContentLength)
+	reader := bufio.NewReaderSize(res.Body, int(res.ContentLength)+1024)
+
+	file, err := os.Create(destPath + fileName)
+	if err != nil {
+		return nil, err
+	}
+	// 获得文件的writer对象
+	writer := bufio.NewWriter(file)
+	io.Copy(writer, reader)
+	file.Seek(0, 0)
+	return file, nil
+}
+
+//PathExists 文件或文件夹是否存在
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+//MakePath 创建目录
+func MakePath(path string, perm os.FileMode) string {
+	if res := PathExists(path); !res {
+		err := os.MkdirAll(path, perm)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return path
+}
